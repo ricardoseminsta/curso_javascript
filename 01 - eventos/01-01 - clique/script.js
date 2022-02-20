1a7532b277b3d2f51104fd5ff952a00c317f8704
ricardo <ricardo.emanuel66@gmail.com>
2022-02-19 22:50:56 -0300

    commit fim manipoulação

--- 01 - eventos/01-01 - clique/script.js
+++ 01 - eventos/01-01 - clique/script.js
@@ -3,8 +3,34 @@ function clicou() {
 }
 
 function digitou(e) {
-    if(e.keyCode == 13 && e.crtlKey == true) {
+    if(e.keyCode == 13 && e.ctrlKey == true) {
         let texto = document.getElementById('campo').value;
         console.log(texto)
     }
+}
+
+function azul(){
+    limpar();
+    document.getElementById('titulo').classList.add('azul');
+}
+
+function vermelho(){
+    limpar();
+    document.getElementById('titulo').classList.add('vermelho');
+}
+
+function verde(){
+    limpar();
+    document.getElementById('titulo').classList.add('verde');
+}
+
+function limpar() {
+    document.getElementById('titulo').classList.remove('vermelho');
+    document.getElementById('titulo').classList.remove('azul');
+    document.getElementById('titulo').classList.remove('verde');
+}
+
+function mostrarTelefone(e) {
+    e.style.display = "none";
+    document.getElementById('telefone').style.display = "block";
 }
