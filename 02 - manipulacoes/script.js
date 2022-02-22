205562ffde51028ade9b7aaf8dade59c1fd1616d
RICARDO EMANUEL FERREIRA SILVA <ricardo.emanuel66@gmail.com>
2022-02-22 16:10:14 -0300

    commit metodos array

---
+++ 02 - manipulacoes/script.js
@@ -0,0 +1,31 @@
+let nome = 'Ricardo Emanuel Silva';
+
+let resultado = nome.split(' ');
+
+// console.log(resultado[0]);
+// console.log(resultado.length);
+
+let n = '10';
+
+// let res = parseInt(n) + 10;
+
+// console.log(res);
+
+// Manipulação de array
+
+let lista = ['ovo', 'farinha', 'corante', 'massa'];
+
+// let res = lista.toString();
+// let res = lista.join("-");
+// let res = lista.indexOf('corante');
+// lista.pop();
+// lista.shift();
+// lista.push('prato')
+// lista[0] = 'ovos';
+lista[4] = 'liquidificador';
+let res = lista
+
+
+
+
+console.log(res);
