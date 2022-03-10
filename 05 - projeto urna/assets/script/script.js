39517a3e4bdeb4a95d774e6a1714bfe9f6ef4274
RICARDO EMANUEL FERREIRA SILVA <ricardo.emanuel66@gmail.com>
2022-03-10 18:49:32 -0300

    commit fim interface

--- 05 - projeto urna/assets/script/script.js
+++ 05 - projeto urna/assets/script/script.js
@@ -15,7 +15,11 @@ function comecarEtapa() {
     let numeroHtml = '';
 
     for(let i=0; i<etapa.numeros; i++) {
-        numeroHtml += '<div class="numero"></div>'
+        if(i === 0){
+            numeroHtml += '<div class="numero pisca"></div>';
+        } else {
+        numeroHtml += '<div class="numero"></div>';
+            }
     }
 
     seuVotoPara.style.display = 'none';
@@ -31,7 +35,14 @@ function atualizaInterface() {
 }
 
 function clicou(n) {
-    alert('clicou em '+n);
+   let elNumero = document.querySelector('.numero.pisca');
+   if(elNumero != null) {
+       elNumero.innerHTML = n;
+       numero = `${numero}${n}`;
+
+       elNumero.classList.remove('pisca');
+       elNumero.nextElementSibling.classList.add('pisca');
+   }
 }
 
 function branco() {
