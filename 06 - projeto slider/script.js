39e9741d50828b82082f6ae69d1ac09f498fc167
RICARDO EMANUEL FERREIRA SILVA <ricardo.emanuel66@gmail.com>
2022-03-15 16:36:07 -0300

    projeto slider

---
+++ 06 - projeto slider/script.js
@@ -0,0 +1,29 @@
+let totalSlides = document.querySelectorAll('.slider--item').length;
+let curretSlide = 0;
+
+document.querySelector('.slider--width').style.width = `calc(100vw1 * ${totalSlides})`;
+document.querySelector('.slider--controls').style.height = `${document.querySelector('.slider').clientHeight}px`;
+
+function goPrev() {
+    curretSlide--;
+    if(curretSlide < 0)
+    {
+        curretSlide = totalSlides -1;
+    }
+    updateMargin();
+}
+
+function goNext() {
+    curretSlide++;
+    if(curretSlide > (totalSlides-1)){
+        curretSlide = 0;
+    }
+    updateMargin();
+}
+
+function updateMargin(){
+    let newMargin = (curretSlide * document.body.clientWidth);
+    document.querySelector('.slider--width').style.marginLeft = `-${newMargin}px`;
+}
+
+setInterval(goNext, 5000);
