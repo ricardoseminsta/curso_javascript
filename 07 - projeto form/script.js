b3733d8d0859776e56549e668a60a4fea667ab08
RICARDO EMANUEL FERREIRA SILVA <ricardo.emanuel66@gmail.com>
2022-03-17 20:14:47 -0300

    commit remove não funciona

--- 07 - projeto form/script.js
+++ 07 - projeto form/script.js
@@ -1,16 +1,18 @@
 let b7validator = {
-    handelSubmit:(event) => {
+    handleSubmit:(event) => {
         event.preventDefault();
         let send = true;
 
         let inputs = form.querySelectorAll('input');
+
+        b7validator.clearErrors();
 
         for(let i=0; i<inputs.length; i++) {
             let input = inputs[i];
             let check = b7validator.checkInput(input);
             if(check !== true) {
                 send = false;
-                console.log(check);
+                b7validator.showError(input, check);
             }
         }
         if(send) {
@@ -36,9 +38,27 @@ let b7validator = {
         }
 
         return true;
+    },
+
+    showError:(input, error) => {
+        input.style.borderColor = 'rgb(156, 30, 30)';
+
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
+    },
+
+    clearErrors:() => {
+
+        let errorElements = document.querySelectorAll('.error');
+        for(let i; i<errorElements.length; i++) {
+            errorElements[i].remove();
+        }
     }
 };
 
 let form = document.querySelector('.b7validator');
 
-form.addEventListener('submit', b7validator.handelSubmit)
+form.addEventListener('submit', b7validator.handleSubmit)
