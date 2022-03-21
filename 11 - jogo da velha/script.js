d4db7bb412eb93d47d1a784a46250e58fa796508
RICARDO EMANUEL FERREIRA SILVA <ricardo.emanuel66@gmail.com>
2022-03-21 12:14:17 -0300

    commit jogo da velha concluido

--- 11 - jogo da velha/script.js
+++ 11 - jogo da velha/script.js
@@ -20,9 +20,10 @@ document.querySelectorAll('.item').forEach(item => {
 // Functions
 function itemClick(event){
     let item = event.target.getAttribute('data-item');
-    if(square[item] === '') {
+    if(playing && square[item] === '') {
         square[item] = player;
         renderSquare();
+        togglePlayer();
     }
 }
 
@@ -43,12 +44,67 @@ function reset() {
 
 function renderSquare(){
     for(let i in square) {
-        let item = document.querySelector(`div[data-item=${i}]`)
+        let item = document.querySelector(`div[data-item=${i}]`);
         item.innerHTML = square[i];
     }
+
+    checkGame();
 }
 
 function renderInfo() {
     document.querySelector('.vez').innerHTML = player;
     document.querySelector('.resultado').innerHTML = warning;
+}
+
+function togglePlayer() {
+    player = (player === 'x') ? 'o' : 'x';
+    renderInfo();
+}
+
+function checkGame() {
+    if(checkWinnerFor('x')) {
+        warning = 'O "x" Venceu!!';
+        playing = false;
+    } else if(checkWinnerFor('o')){
+        warning = 'O "o" Venceu!!!';
+        playing = false;
+    } else if(isFull()) {
+        warning = 'Deu empate';
+        playing = false;
+    }
+}
+
+function checkWinnerFor(player) {
+    let pos = [
+        'a1,a2,a3',
+        'b1,b2,b3',
+        'c1,c2,c3',
+
+        'a1,a1,c1',
+        'a1,b2,c2',
+        'a3,b3,c3',
+
+        'a1,b2,c3',
+        'a3,b2,c1'
+    ];
+
+    for(let w in pos) {
+        let pArray = pos[w].split(','); // a1, a2, a3
+        let hasWon = pArray.every(option => square[option] === player)
+        if(hasWon) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function isFull() {
+    for(let i in square) {
+        if(square[i] === '') {
+            return false;
+        }
+    }
+
+    return true;
 }
